8b2a9df44d67061dbdfbff94077368fa26465385
Eugene Kimathi <eugenekimathi@gmail.com>
2026-07-29 23:39:39 +0300

    Add Model Constraints and Validation

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 from flask import Flask
-from app import db
-import app.models
+from app import create_app, db
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///parking.db"
-db.init_app(app)
+app = create_app()
 
-with app.app_context():
-    db.create_all()   
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
